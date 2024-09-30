ed1de90d51f80ead1f2a0fb15fe8d88155adb11c
Grace Purcell <gracecpurcell2695@gmail.com>
2024-09-30 17:38:04 +0200

    rds database and variables

---
+++ rds/variables.tf
@@ -0,0 +1,27 @@
+variable "db-name" {
+    type = string
+}
+variable "db-username" {
+    type = string
+}
+variable "db-password" {
+    type = string
+}
+variable "db-engine" {
+    type = string
+}
+variable "db-engine-version" {
+    type = string
+}
+variable "db-instance-class" {
+    type = string
+}
+variable "db-parameter-group-name" {
+    type = string
+}
+variable "db-allocated-storage" {
+    type = number
+}
+variable "db-max-storage" {
+    type = number
+}
